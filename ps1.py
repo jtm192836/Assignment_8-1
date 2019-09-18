8a59d5ae7f27904cff258d34cb64fd4552a79afd
jtm192836 <55388137+jtm192836@users.noreply.github.com>
2019-09-18 15:56:46 +0530

    Add files via upload

--- ps1.py
+++ ps1.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+#parity-check
+#input binary message
+input_bin =str(input ("Enter binary bit data that has to be transmitted:"))
+#Defining a function to count no. of occurrences of 1
+count = 0
+for i in input_bin: 
+	if i == '1': 
+		count = count + 1
+#taking binary digits as input 
+#count checking if even or odd
+#n=len(bin_list)
+if (count%2==0):
+	input_bin = input_bin + "1"
+	#bin_list = bin_list.split("")
+	print ("Parity bit data:{0}".format(input_bin))
+else:
+	input_bin = input_bin + "0"
+	#bin_list = bin_list.split("")
+	print ("Parity bit data:{0}".format(input_bin))
 
+#Bit-oriented framing
+string = input_bin
+   
+# Prints the string by replacing only 3 occurence  
+str2 = string.replace("010", "0100", 3)
+#appending 0101 at end
+str1=str2+"0101"
+print("Transmitting data:{0}".format(str1))
--- ps2.py
+++ ps2.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
+print("Welcome to the Game!")
+def drawsmall():
+	a = (' ___' *  3 )
+	b = '   '.join('||||')
+	print('\n'.join((a, b, a, b, a, b, a, )))
+
+drawsmall()
+list1 =[0,0,0,
+	0,0,0,
+	0,0,0]
+print(list1)
+print("Player 1's chance")
+pos,num=input("Enter the position and number to be entered:").split(",")
+
+print("Player 2's chance")
+pos,num=input("Enter the position and number to be entered:").split(",")
+
 
